4af6d33b4fcec9ef14ad7cbdb1bf08b860321b5c
PCdurham <patnpetunia@gmail.com>
2020-03-23 09:25:38 +0000

    Update CNNSupervisedClassification_ice.py

--- code/CNNSupervisedClassification_ice.py
+++ code/CNNSupervisedClassification_ice.py
@@ -67,11 +67,13 @@ import glob
 """User data input. Fill in the info below before running"""
 #############################################################
 
-ModelName = 'Train22432VGG16_32stride13ims5eps'     #should be the model name from previous run of TrainCNN.py
-TrainPath = 'D:\\CNN_Data\\'  
-PredictPath = 'D:\\S2_Images\\'    #Location of the images
-IndividualValidTile = 'D:\\S2_Images\\SCLS_S2A1.tif\\'
-ScorePath = 'D:\\S2_Images\\'      #location of the output files and the model
+ModelName = 'Train10035VGG16_10035stride13ims5eps'     #should be the model name from previous run of TrainCNN.py
+size = 224 #size of tiles in the model
+
+TrainPath = 'E:\SEE_ICE\\'  
+PredictPath = 'E:\\SEE_ICE\\S2_Images\\'    #Location of the images
+IndividualValidTile = 'E:\\SEE_ICE\\S2_Images\\\\SCLS_S2A1.tif'
+ScorePath = 'E:\\SEE_ICE\\S2_Images\\Results\\'      #location of the output files and the model
 Experiment = 'Test1CSC'    #ID to append to output performance files
 
 '''BASIC PARAMETER CHOICES'''
@@ -336,7 +338,6 @@ ConvNetmodel = load_model(FullModelPath)
 
 ###############################################################################
 """Classify the holdout images with CNN-Supervised Classification"""
-size = 224 #Do not edit. The base models supplied all assume a tile size of 50.
 
 # Getting River Names from the files
 # Glob list fo all jpg images, get unique names form the total list
