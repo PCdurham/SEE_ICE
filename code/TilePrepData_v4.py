83da5724da46ca60ae7e4e412947a8230b2c4fd4
MMarochov <melanie.marochov@durham.ac.uk>
2020-09-30 17:13:59 +0100

    Update TilePrepData_v4.py

--- code/TilePrepData_v4.py
+++ code/TilePrepData_v4.py
@@ -1,45 +1,40 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jan 15 12:02:30 2020
-@author: Melanie Marochov
+@author: Melanie Marochov and Patrice Carbonneau
 
 
-    TILE PREPARATION
+Name:           Tile Preparation - for CSC Application
+Compatibility:  Python 3.6
+Description:    Tiles images into manageable 4D stacks of image bands (R,G,B,NIR)
+                for applying the CNN-Supervised Classification (CSC) workflow.
 
 """
 # =============================================================================
 
-"""Import Libraries"""
+""" Import Libraries """
 
 import numpy as np
-import pandas as pd
 import skimage.io as IO
-import sys
+
 # =============================================================================
 
-"""User Input"""
+""" User Input - Fill in the info below before running """
 
 ImName = 'H13_09_19RGBN.tif' #name of image to be tiled
-ClassName = 'H13_09_19V.tif' #name of class raster used to assign classes to tiles
-ImFolder = 'E:\\Masters\\Helheim19\\g_13_09\\' #location of image to be tiled
+ClassName = 'H13_09_19V.tif' #name of class raster used to assess accuracy
+ImFolder = 'E:\\Masters\\Helheim19\\g_13_09\\' # folder location of image to be tiled
 DataFolder = 'D:\\S2_Images\\' #folder location for output tiles
 RootName = 'S2A'
 
 size = 3000 #size (in pixels) of output tiles
-LastTile =0 #last tile number from previous image
+LastTile = 0 #last tile number from previous image (prevents overwriting files)
 
 # =============================================================================
-###############################################################################
 
-"""Functions"""
+""" Helper Functions """
 
-#### CHECKLABEL FUNCTION #### 
-#Checks class percentage of each tile and creates the label vector.
-#Tiles which contain less than 10% of pure class, or a mixture of classes are rejected
-#Label vectors are produced from tiles which contain >=90% pure class.
-
-###############################################################################
-    
+    # CropToTile Function
 # Helper function to crop images to have an integer number of tiles. No padding is used.
 def CropToTile (Im, size):
     if len(Im.shape) == 2:#handle greyscale
@@ -50,18 +45,19 @@ def CropToTile (Im, size):
     return Im[0:crop_dim0, 0:crop_dim1, :]
     
 
-###############################################################################
-    #Save image tiles to disk based on their associated class 
+# =============================================================================
+
+    #Save image tiles to disk  
+
 def save_tile(RasterTile, CurrentTile, DataFolder, RootName):
-    TileName = DataFolder+RootName+str(CurrentTile) + '.png'
+    TileName = DataFolder+RootName+str(CurrentTile) + '.png' #saves tile as .png
     IO.imsave(TileName, RasterTile)
 
 
-####################################################################################
+# =============================================================================
 # =============================================================================
 
 """Processing"""
-#Tile sliding- extraction with a stride of 1
 
 #Load image
 Im3D = IO.imread(ImFolder+ImName)
@@ -69,8 +65,8 @@ ClassRaster = IO.imread(ImFolder+ClassName)
 
 
 #Tile Processing
-#im is image ready to be tiled
-im = CropToTile (Im3D, size)
+
+im = CropToTile (Im3D, size) #im is image ready to be tiled
 CroppedClassRaster = CropToTile (ClassRaster, size)
 
 
@@ -81,15 +77,13 @@ else:
     h, w, d = im.shape
 
 CurrentTile = LastTile+1
-for y in range(0, h,size): #from first pixel to last pixel where 224 will fit, in steps of stride
+for y in range(0, h,size): #from first pixel to last pixel where tile size will fit
     for x in range(0, w,size):
         LabelTile = CroppedClassRaster[y:y+size,x:x+size] #Cropped class raster to tile size
         Tile = im[y:y+size,x:x+size,:].reshape(size,size,d) # image tile
-        Tile = np.uint8(255*Tile/16384) # used to be 16384 but replaced with max of image used.
+        Tile = np.uint8(255*Tile/16384) # Normalises tile.
         save_tile(Tile, CurrentTile, DataFolder, RootName) #Save the tile to disk
         save_tile(LabelTile, CurrentTile, DataFolder, 'SCLS_'+RootName) #Save the tile to disk
-        CurrentTile+=1 #saves rotated tile and does not overwrite previously saved files
-        #save_tile(Tile, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk
+        CurrentTile+=1 #to prevent overwriting previously saved files
 
 print('Biggest valid tile was '+str(CurrentTile))
-#TileName = 'T'+str(CurrentTile) + '.jpg'
