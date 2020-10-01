1b4d479e1bfc12259e5e60a49b721daa704a730f
MMarochov <60142411+MMarochov@users.noreply.github.com>
2020-10-01 01:22:39 +0100

    Update Phase1_VGG16_RGB-RGBNIR.py

--- code/Phase1_VGG16_RGB-RGBNIR.py
+++ code/Phase1_VGG16_RGB-RGBNIR.py
@@ -35,20 +35,20 @@ import sys
 
 """ User Inputs - Fill in the info below before running """
 
-#Trained model will also be written out to the training folder
-train_path = 'path_e.g_E:\\See_Ice\\Train' #where training tiles are located
-valid_path = 'path' #where validation tiles are located - use \\ instead of \ in path names
-TileSize = 100  #size of tiles created using TilePreparation_CNNTrainingData 
-Nbands=3    #number of input bands (3 for RGB or 4 for RGB+NIR)
-Nclasses=7  #number of semantic classes
-BaseFilters=32  #number of filters
-training_epochs = 15    #number of epochs model will iterate over training data
-ImType='.png'   #png, jpg, or tif
-ModelTuning = False     #set to True if you need to tune the training epochs. Remember to lengthen the epochs
-TuningFigureName = 'empty'  #name of the tuning figure, no need to add the path
+#Trained model will also be written out to the training folder.
+train_path = 'path'   #where training tiles are located e.g. 'E:\\See_Ice\\Train'.
+valid_path = 'path'   #where validation tiles are located - use \\ instead of \ in path names.
+TileSize = 100        #size of tiles created using TilePreparation_CNNTrainingData.
+Nbands=3              #number of input bands (3 for RGB or 4 for RGB+NIR).
+Nclasses=7            #number of semantic classes.
+BaseFilters=32        #number of filters.
+training_epochs = 15  #number of epochs model will iterate over training data.
+ImType='.png'         #png, jpg, or tif.
+ModelTuning = False   #set to True if you need to tune the training epochs. Remember to lengthen the epochs.
+TuningFigureName = 'empty'  #name of the tuning figure, no need to add the path.
 learning_rate = 0.0001
 verbosity = 1
-ModelOutputName = 'empty'  #name of trained model
+ModelOutputName = 'empty'   #name of trained model.
 
 # =============================================================================
 
