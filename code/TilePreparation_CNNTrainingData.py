70f7108effeb8f206e8f197c6f96436f77085fcd
MMarochov <60142411+MMarochov@users.noreply.github.com>
2020-10-01 01:17:59 +0100

    Update TilePreparation_CNNTrainingData.py

--- code/TilePreparation_CNNTrainingData.py
+++ code/TilePreparation_CNNTrainingData.py
@@ -24,10 +24,10 @@ import glob
 
 """ User Input - Fill in the info below before running """
 
-ImFolder = 'path_e.g_E:\\See_Ice\\TrainData\\' #location of image to be tiled
-DataFolder = 'path' #folder location for output tiles
-size = 100 #size (in pixels) of output tiles
-stride = 35 #number of pixels the tiler slides before extracting another tile
+ImFolder = 'path'    #location of image to be tiled e.g. 'E:\\See_Ice\\TrainData\\'.
+DataFolder = 'path'  #folder location for output tiles.
+size = 100           #size (in pixels) of output tiles.
+stride = 35          #number of pixels the tiler slides before extracting another tile.
 
 
 # =============================================================================
@@ -36,21 +36,21 @@ stride = 35 #number of pixels the tiler slides before extracting another tile
 
     # CheckLabel Function 
 #Checks class percentage of each tile and creates the label vector.
-#Tiles which contain less than 10% of pure class, or a mixture of classes are rejected
+#Tiles which contain less than 10% of pure class, or a mixture of classes are rejected.
 #Label vectors are produced from tiles which contain >=90% pure class.
 
 def CheckLabel(ClassTile):
     
     size=ClassTile.shape[0] #gets the size of the tile
     vals, counts = np.unique(ClassTile, return_counts = True)
-    if (vals[0] == 0) and (counts[0] > 0.1 * size**2): #if only 10% of the tile has a class
-        Valid = False #This identifies the tile as non-classified (so it will reject it)
-    elif counts[np.argmax(counts)] >= 0.95 * size**2: #if biggest class is over 95% of area valid is True
-        Valid = True #This identifies the class of the tile 
+    if (vals[0] == 0) and (counts[0] > 0.1 * size**2): #If only 10% of the tile has a class.
+        Valid = False #This identifies the tile as non-classified (so it will reject it).
+    elif counts[np.argmax(counts)] >= 0.95 * size**2: #If biggest class is over 95% of area valid is True.
+        Valid = True #This identifies the class of the tile. 
     else:
-        Valid = False #mix of classes that add up to 90% area
+        Valid = False #mix of classes that add up to 90% area.
    
-    return Valid #Given a classification tile, runs the check for class label
+    return Valid #Given a classification tile, runs the check for class label.
 
 # =============================================================================
     
@@ -58,7 +58,7 @@ def CheckLabel(ClassTile):
 # Crops images to have an integer number of tiles. No padding is used.
     
 def CropToTile (Im, size):
-    if len(Im.shape) == 2: #handle greyscale
+    if len(Im.shape) == 2: #handle greyscale.
         Im = Im.reshape(Im.shape[0], Im.shape[1],1)
 
     crop_dim0 = size * (Im.shape[0]//size)
@@ -72,9 +72,9 @@ def CropToTile (Im, size):
 #Save image tiles to disk based on their associated class 
     
 def save_tile(I, LabelVector, CurrentTile, DataFolder, size, stride):
-    PickFolder = np.random.uniform() #Picks a random number to allocate isolated tiles to folder (uniform between 0 and 1)
+    PickFolder = np.random.uniform() #Picks a random number to allocate isolated tiles to folder (uniform between 0 and 1).
     TileName = 'T'+str(CurrentTile) + '.png'
-    if PickFolder <= 0.95: #For distributing in train and test folders
+    if PickFolder <= 0.95: #For distributing in train and test folders.
         if LabelVector== 1:
             IO.imsave(DataFolder+'Train'+'\\C1\\'+TileName, I)
 
@@ -144,33 +144,33 @@ for i in range(len(img)):
         h, w, d = im.shape
     
     
-    for y in range(0, h-size, stride): #from first pixel to last pixel where tile size will fit, in steps of stride
+    for y in range(0, h-size, stride): #From first pixel to last pixel where tile size will fit, in steps of stride.
         for x in range(0, w-size, stride):
-            LabelTile = CroppedClassRaster[y:y+size,x:x+size] #Cropped class raster to tile size
-            Label = np.median(CroppedClassRaster[y:y+size,x:x+size].reshape(1,-1)) #class expressed as a single number for an individual tile
+            LabelTile = CroppedClassRaster[y:y+size,x:x+size] #Cropped class raster to tile size.
+            Label = np.median(CroppedClassRaster[y:y+size,x:x+size].reshape(1,-1)) #Class expressed as a single number for an individual tile.
             Valid = CheckLabel(LabelTile)
-            Tile = im[y:y+size,x:x+size,:].reshape(size,size,d) # image tile
+            Tile = im[y:y+size,x:x+size,:].reshape(size,size,d) #Image tile.
             Tile = np.uint8(255*Tile/16384)
-            if Valid: #==true i.e. if the tile has a dominant class assigned to it
+            if Valid: #==true i.e. if the tile has a dominant class assigned to it.
                 #raw tile
                 I=Tile
-                save_tile(I, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk
-                CurrentTile+=1 #current tile plus 1 - so won't overwrite previously saved file
-                #90 rotation + noise
+                save_tile(I, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk.
+                CurrentTile+=1 #Current tile plus 1 - so won't overwrite previously saved file.
+                #90 rotation + noise.
                 Tile=np.rot90(Tile)
-                I=Tile+np.uint8(3*np.random.uniform(size=Tile.shape)) #rotates tile 90 degrees + noise from 0-2
-                save_tile(I, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk
-                CurrentTile+=1 #saves rotated tile and does not overwrite previously saved files
-                #180 rotation + noise
+                I=Tile+np.uint8(3*np.random.uniform(size=Tile.shape)) #Rotates tile 90 degrees + noise from 0-2.
+                save_tile(I, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk.
+                CurrentTile+=1 #Saves rotated tile and does not overwrite previously saved files.
+                #180 rotation + noise.
                 Tile=np.rot90(Tile)
-                I=Tile+np.uint8(3*np.random.uniform(size=Tile.shape)) #rotates tile 90 degrees+ noise from 0-2
-                save_tile(I, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk
-                CurrentTile+=1 #saves rotated tile and does not overwrite previously saved files
-               #270 rotation + noise
+                I=Tile+np.uint8(3*np.random.uniform(size=Tile.shape)) #Rotates tile 90 degrees+ noise from 0-2.
+                save_tile(I, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk.
+                CurrentTile+=1 #Saves rotated tile and does not overwrite previously saved files.
+               #270 rotation + noise.
                 Tile=np.rot90(Tile)
-                I=Tile+np.uint8(3*np.random.uniform(size=Tile.shape)) #rotates tile 90 degrees+ noise from 0-2
-                save_tile(I, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk
-                CurrentTile+=1 #saves rotated tile and does not overwrite previously saved files
+                I=Tile+np.uint8(3*np.random.uniform(size=Tile.shape)) #Rotates tile 90 degrees+ noise from 0-2.
+                save_tile(I, Label, CurrentTile, DataFolder, size, stride) #Save the tile to disk.
+                CurrentTile+=1 #Saves rotated tile and does not overwrite previously saved files.
                 
 
 
